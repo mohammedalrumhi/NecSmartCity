0f2ccc79b075e4d022e5379f675a1673afadcee9
moham <mohammed.alrumhi@outlook.com>
2020-02-20 11:03:28 +0400

    Hello smart city

---
+++ app/src/main/java/com/example/necsmartcity/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.necsmartcity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.layout_home);
+  /// we start codding smart city in the middle of the dat
+        // at this point w are starting codding the layout that avavliable to user
+
+        //this screen is to user that allow to user to login with the application.
+
+
+
+    }
+
+
+}
